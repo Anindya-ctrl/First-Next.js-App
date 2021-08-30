6c6bc284c0becd043da2387850b2bd561646210a
Anindya <ganpachirokamaboko@gmail.com>
2021-08-30 23:20:09 +0600

    fixed status code for error

--- pages/api/waifus/[id].js
+++ pages/api/waifus/[id].js
@@ -3,7 +3,7 @@ import dummyData from "../../../dummyData";
 function handler(req, res) {
     const { id } = req.query;
 
-    if(id > dummyData.length) return res.status(200).json({ error: 'Invalid id provided.' });
+    if(id > dummyData.length) return res.status(404).json({ error: 'Invalid id provided.' });
 
     return res.status(200).json(dummyData[id - 1]);
 }
