a65b70e1d4639dca3a5b44dd25e50667165d351a
Anindya <ganpachirokamaboko@gmail.com>
2021-08-30 20:58:22 +0600

    changed 404 image configs

--- pages/404.jsx
+++ pages/404.jsx
@@ -1,9 +1,15 @@
 import Link from 'next/link';
+import Image from 'next/image';
 
 function NotFound() {
     return (
         <div className="not-found">
-            <img src="https://anilist.co/img/404/404_chan1.jpg" alt="" />
+            <Image
+                src="https://anilist.co/img/404/404_chan1.jpg"
+                height={ 800 }
+                width={ 1000 }
+                alt="404-chan"
+            />
 
             <p>
                 Go back to <Link href="/"><a>Home</a></Link>
